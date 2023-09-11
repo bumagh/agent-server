06d41336d283d8a27704bf2906d1281e13f59d05
xsx <ddh619174120@gmail.com>
2023-09-11 12:02:52 +0800

    t

--- app/api/agent/admin.py
+++ app/api/agent/admin.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 from flask import Blueprint
-
+from app.core.utils import jsonify
 from app.core.error import Success
 from app.core.utils import paginate
 from app.dao.admin import AdminDao
+from app.dao.admin_group_access import AdminGroupAccessDao
+from app.model.baAdmin import BaAdmin
+from app.model.baAdminGroup import BaAdminGroup
 from app.service.admin import AdminService
 
 admin = Blueprint('admin', __name__, url_prefix='/admin')
@@ -19,8 +22,23 @@ def get_user_list():
     return Success(rv)
 
 
-@admin.route('/<int:uid>', methods=['GET'])
+@admin.route('/edit/<int:uid>', methods=['GET'])
 def get_user(uid):
     '''查询用户信息'''
-    user = AdminDao.query.filter_by(id=uid).first_or_404()
+    user = BaAdmin.query.filter_by(id=uid).first_or_404()
+    groupid, groupname = AdminGroupAccessDao.get_user_admin_group_info(user['id'])
+    user = jsonify(user)
+    user['group_arr'] = groupid
+    user['group_name_arr'] = groupname
     return Success(user)
+
+
+@admin.route('/rule_list/<int:id>', methods=['GET'])
+def rule_list(id):
+    '''查询分组信息'''
+    query = BaAdminGroup.query
+    if id > 0:
+        query = BaAdminGroup.query.filter_by(id=id).first_or_404()
+    else:
+        query = query.all()
+    return Success(query)
